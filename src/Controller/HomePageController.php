3218ffdacded17f2130a3a940395cd980d2dfcb5
Refaeltz <Refaeltz@gmail.com>
2019-08-01 13:05:41 +0300

    first controller added

---
+++ src/Controller/HomePageController.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Refael-laptop
+ * Date: 01/08/2019
+ * Time: 12:02
+ */
+
+namespace Controller;
+
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class HomePageController
+{
+	/**
+	 * @Route("/")
+	 */
+	public function homepage(){
+
+	}
+}
